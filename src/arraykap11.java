95c877db01ac7fe27600669bc710a4e37fc04b64
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2018-11-28 11:08:03 +0100

    Fixade ett fel (i+i) blev till (1+i)

--- src/arraykap11.java
+++ src/arraykap11.java
@@ -7,14 +7,15 @@ int n = Integer.parseInt(s);
 String [] namn = new String [n];
 double [] pris = new double [n];
 for (int i=0; i<n; i++) {
-    namn[i] = JOptionPane.showInputDialog("Name person number " + (i+i) + "?");
+    namn[i] = JOptionPane.showInputDialog("Name person number " + (1+i) + "?");
     s = JOptionPane.showInputDialog("Score on the test for " + namn[i] + "?");
     pris[i] = Double.parseDouble(s);
 }
 int worst = 0;
 for (int i=1; i<n; i++)
-    if (pris[i] < pris[worst])
+    if (pris[i] < pris[worst]) {
         worst = i;
+    }
     JOptionPane.showMessageDialog(null, namn[worst] + " was the worst at the test. \n" + "Score: " + pris[worst] + " poäng.");
     }
 }
